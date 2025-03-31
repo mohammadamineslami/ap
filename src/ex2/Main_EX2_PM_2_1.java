baefaadc72964d9be40cac13a763977e4a1e9d91
mohammadamineslami <spidaamins@gmail.com>
2025-03-31 16:18:40 +0330

    ex2_pm_1

---
+++ src/ex2/Main_EX2_PM_2_1.java
@@ -0,0 +1,29 @@
+package ex2;
+
+import java.util.Scanner;
+
+public class Main_EX2_PM_2_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("please enter k:gi");
+        int k = scanner.nextInt();
+        int size = k + 2;
+        char[][] array = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+                    array[i][j] = '*';
+                } else {
+                    array[i][j] = ' ';
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
